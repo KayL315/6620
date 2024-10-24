7f7eab945b1dd9111e4178ea4762c6fab7c56b2f
Kay <127446653+KayL315@users.noreply.github.com>
2024-10-24 03:53:47 -0700

    Update driver_lambda.py

--- driver_lambda.py
+++ driver_lambda.py
@@ -1,12 +1,13 @@
 import boto3
 import time
+import requests
 
 s3_client = boto3.client('s3', region_name='us-west-2')
 lambda_client = boto3.client('lambda', region_name='us-west-2')
 
 def lambda_handler(event, context):
     bucket_name = 'a2testbucket123'
-
+#    api_url = 'https://xxxx.execute-api.us-west-2.amazonaws.com/prod/plot'
     s3_client.put_object(Bucket=bucket_name, Key='assignment1.txt', Body='Empty Assignment 1')
     time.sleep(5)
     s3_client.put_object(Bucket=bucket_name, Key='assignment1.txt', Body='Empty Assignment 2222222222')
@@ -17,8 +18,20 @@ def lambda_handler(event, context):
     
     s3_client.put_object(Bucket=bucket_name, Key='assignment2.txt', Body='33')
     
+    
+#    s3_client.put_object(Bucket=bucket_name, Key='size/assignment1.txt', Body='Empty Assignment 1')
+#    time.sleep(5)
+#    s3_client.put_object(Bucket=bucket_name, Key='size/assignment1.txt', Body='Empty Assignment 2222222222')
+#    time.sleep(5)
+#    
+#    s3_client.delete_object(Bucket=bucket_name, Key='size/assignment1.txt')
+#    time.sleep(5)
+#    
+#    s3_client.put_object(Bucket=bucket_name, Key='size/assignment2.txt', Body='33')
+    
 
     lambda_client.invoke(FunctionName='plottingLambda')
+#    response = requests.post(api_url)
     
     return {
         'statusCode': 200,
