5f75735e1040bad98f023176d5349d9fe7d62eca
Kay <127446653+KayL315@users.noreply.github.com>
2024-10-24 03:54:28 -0700

    Update plotting_lambda.py

--- plotting_lambda.py
+++ plotting_lambda.py
@@ -1,5 +1,6 @@
 import boto3
 import matplotlib.pyplot as plt
+from boto3.dynamodb.conditions import Key
 import time
 import io
 
@@ -7,17 +8,20 @@ dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 s3 = boto3.client('s3', region_name='us-west-2')
 
 def lambda_handler(event, context):
-    bucket_name = 'a2testbucket123' 
+    bucket_name = 'a2testbucket123'
     table_name = 'S3-object-size-history'
 
     table = dynamodb.Table(table_name)
     current_time = int(time.time())
-    start_time = current_time - 10
+    start_time = current_time - 2500
+
     response = table.query(
-    KeyConditionExpression=boto3.dynamodb.conditions.Key('BucketName').eq('a2testbucket123') &
-                           boto3.dynamodb.conditions.Key('Timestamp').between(str(start_time), str(current_time))
-)
-    
+        KeyConditionExpression=Key('BucketName').eq(bucket_name) &
+                               Key('Timestamp').between(str(start_time), str(current_time))
+    )
+
+    print(f"DynamoDB query result: {response['Items']}")
+
     timestamps = []
     sizes = []
     max_size = 0
@@ -26,21 +30,38 @@ def lambda_handler(event, context):
         timestamps.append(int(item['Timestamp']))
         sizes.append(item['TotalSize'])
         max_size = max(max_size, item['TotalSize'])
-    
-    # Plotting
+
+    print(f"Plot data - sizes: {sizes}, timestamps: {timestamps}")
+
+    if not timestamps:
+        print("No data retrieved from DynamoDB.")
+        return {
+            'statusCode': 400,
+            'body': 'No data to plot.'
+        }
+
     plt.figure()
-    plt.plot(timestamps, sizes, label='Bucket Size')
+    plt.plot(timestamps, sizes, label='Bucket Size',color='blue', linewidth=2, marker='o', markersize=8)
+    
     plt.axhline(y=max_size, color='r', linestyle='--', label='Max Size')
+
+    plt.ylim(0, max_size + 5000)
+
     plt.xlabel('Timestamp')
     plt.ylabel('Bucket Size (Bytes)')
+    plt.xticks(rotation=45)
     plt.legend()
-    
+
     buffer = io.BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
-    s3.put_object(Bucket='a2testbucket123', Key='plot.png', Body=buffer)
+    
+    print("Saving plot to S3...")
+    s3.put_object(Bucket=bucket_name, Key='plot.png', Body=buffer, ContentType='image/png')
+#    s3_client.put_object(Bucket=bucket_name, Key='plot/plot.png', Body=buffer, ContentType='image/png')
+    print("Plot saved to S3.")
     
     return {
         'statusCode': 200,
         'body': 'Plot saved to S3.'
-    }  
+    }
